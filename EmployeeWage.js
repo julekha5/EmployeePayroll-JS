f639eb04665d77d4ed5c909b13bf73180f7d9e4a
Julekha5 <mulani.julekha93@gmail.com>
2022-01-16 15:47:42 +0530

    Added : Calculate employee month wage for 20 days

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,22 +1,31 @@
 // UC3 write function for daily working hours
-    const IS_PART_TIME = 1;
-    const IS_FULL_TIME = 2;
-    const PART_TIME_HOURS = 4;
-    const FULL_TIME_HOURS = 8;
-    const WAGE_PER_HOUR = 20;
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const PART_TIME_HOURS = 4;
+const FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
-    function getWorkingHours(empCheck) {
-        switch (empCheck) {
-            case IS_PART_TIME:
-                return PART_TIME_HOURS;
-            case IS_FULL_TIME:
-                return FULL_TIME_HOURS;
-            default:
-                return 0;
-        }
+
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
     }
-    let empCheck = Math.floor(Math.random() * 10) % 3;
-    let empHrs = getWorkingHours(empCheck);
-    let empWage = empHrs * WAGE_PER_HOUR;
-    console.log(" Emp Hour:" + empHrs + " Emp Wage: " + empWage);
-
+}
+/**
+ * Calculating wages for a month
+ */
+{
+    let totalEmpHrs = 0;
+    for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log("Total Hours:" + totalEmpHrs + " Employee Wage:" + empWage);
+}
